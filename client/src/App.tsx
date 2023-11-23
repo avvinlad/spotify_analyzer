e22f0429b309858f547259f04fad7ff8a7cdcc8f
Avin Lad <avinlad@gmail.com>
2023-11-23 02:08:56 -0500

    format changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,26 @@
-import React, { FC } from 'react';
-import Login from './Login';
-import Dashboard from './screens/Dashboard';
-import Playlist from './screens/Playlist';
-import Error from './screens/Error';
+import React, { FC } from "react";
+import Login from "./Login";
+import Dashboard from "./screens/Dashboard";
+import Playlist from "./screens/Playlist";
+import Error from "./screens/Error";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-const code: string = new URLSearchParams(window.location.search).get('code') ?? '';
+const code: string =
+  new URLSearchParams(window.location.search).get("code") ?? "";
 
 const App: FC = () => {
-	return (
-		<BrowserRouter>
-		<Routes>
-		<Route path="/" element={code ? <Dashboard code={code}/> : <Login/>}/>
-		<Route path="/playlist/:playlistID" element={<Playlist/>}/>
-		<Route path="*" element={<Error/>}/>
-		</Routes>
-		</BrowserRouter> 
-		);
-	}
-	
-	export default App
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={code ? <Dashboard code={code} /> : <Login />}
+        />
+        <Route path="/playlist/:playlistID" element={<Playlist />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
--- client/src/screens/Playlist.tsx
+++ client/src/screens/Playlist.tsx
@@ -6,260 +6,378 @@ import { useParams } from "react-router-dom";
 import Axios from "axios";
 
 interface PlaylistObj {
-	id: string;
-	name: string;
-	description: string;
-	uri: string;
-	total: number;
-};
+  id: string;
+  name: string;
+  description: string;
+  uri: string;
+  total: number;
+}
 
 interface Track {
-	id: string;
-	name: string;
-	artists: string;
-	dateAdded: string;
-	tempo: number;
-	acousticness: number;
-	valence: number;
-	energy: number;
-	danceability: number;
-	mode: number;
-	key: number;
-	selected: boolean;
-};
+  id: string;
+  name: string;
+  artists: string;
+  dateAdded: string;
+  tempo: number;
+  acousticness: number;
+  valence: number;
+  energy: number;
+  danceability: number;
+  mode: number;
+  key: number;
+  selected: boolean;
+}
 
 interface Artist {
-	id: string; 
-	name: string;
+  id: string;
+  name: string;
 }
 
 const Playlist: FC = () => {
-	let { playlistID } = useParams();
-	const [tracks, setTracks] = useState<Track[]>([]);
-	const [playlist, setPlaylist] = useState<PlaylistObj>();
-	const [sortTracksOrder, setSortTracksOrder] = useState(0);
-	const [selectedTracks, setSelectedTracks] = useState<Track[]>([]);
-	const sortOrder = (filter: string) => sortTracks(filter);
-	const accessToken = sessionStorage.getItem("accessToken");
-	
-	useEffect(() => {
-		if (accessToken && playlistID) {
-			Axios.get("http://localhost:3001/getPlaylistInfo", { params: { accessToken, playlistID }})
-			.then((res) => {
-				const pl: PlaylistObj = {
-					id: res.data.id,
-					name: res.data.name,
-					description: res.data.description,
-					uri: res.data.uri,
-					total: res.data.tracks.total
-				};
-				setPlaylist(pl);
-			})
-			.catch(() => {
-				window.location.href = "/";
-			});
-		}
-	}, [accessToken, playlistID]);
-	
-	
-	useEffect(() => {
-		if (playlist && tracks && tracks.length < playlist.total) {
-			const totalTracks = playlist?.total;
-			Axios.get("http://localhost:3001/getPlaylistTracks", { params: { accessToken, playlistID, totalTracks } })
-			.then((res) => {
-				formatTracks(res.data);
-			})
-			.catch(() => {
-				window.location.href = "/";
-			});
-		}
-	});
-	
-	const handleChange = (track: Track) => {
-		track.selected = !track.selected;
-		if (!track.selected) { 
-			setSelectedTracks(selectedTracks => selectedTracks.filter(curTrack => { return curTrack.id !== track.id }));
-		} else {
-			let found = selectedTracks.findIndex(curTrack => curTrack.id === track.id);
-			if (found === -1) { selectedTracks.push(track); }
-		}
-	};
-	
-	const handleSubmit = async () => {
-		createPlaylist();
-	};
-	
-	function formatTracks(resTracks: any) {
-		let shapedTracks: Track[] = [];
-		shapedTracks = resTracks.map((track: any) => ({
-			id: track.track.id,
-			name: track.track.name,
-			artists: _formatArtists(track.track.artists),
-			dateAdded: track.added_at,
-			tempo: 0,
-			acousticness: 0,
-			valence: 0,
-			energy: 0,
-			danceability: 0,
-			mode: 0,
-			key: 0
-		}));
-		audioFeatures(shapedTracks);
-	}
-	
-	// shape artsits 
-	function _formatArtists(artists: any) {
-		return (artists.map((artist: any) : Artist => ({
-			id: artist.id,
-			name: artist.name
-		}))
-		)
-	}
-	
-	// async processes to retrieve all audio features
-	async function audioFeatures(rawTracks: Track[]) {
-		if (playlist) {
-			let tracks = rawTracks.map(track => track.id)
-			let offsets = Math.floor(tracks.length / 100) + 1;
-			let promises = [];
-			let curTracks: string = '';
-			for (let chunk = 0; chunk < offsets; chunk++) {
-				curTracks = tracks.slice(chunk * 100, (chunk + 1) * 100).join(",");
-				promises.push(_getAudioFeatures(curTracks));
-			}
-			
-			Promise.all(promises)
-			.then((response) => {
-				let audioFeatures: any = [];
-				response.forEach(audioFeature => {
-					audioFeatures = audioFeatures.concat(audioFeature);
-				});
-				addAudioFeatures(rawTracks, audioFeatures);
-			});
-		}
-	}
-	
-	// get request for audio features
-	async function _getAudioFeatures(tracksID: string) {
-		let res = await Axios.get("http://localhost:3001/getTrackFeatures", { params: { accessToken, tracksID } })
-		if (res.status !== 200) { return null; }
-		return res.data;
-	}
-	
-	// add audio features to state
-	function addAudioFeatures(shapedTracks: Track[], audioFeatures: any) {
-		let updatedTracks: Track[] = shapedTracks;
-		if (!tracks && !audioFeatures) { return null; }
-		if (updatedTracks.length !== audioFeatures.length) { return null; }
-		updatedTracks.forEach(track => {
-			audioFeatures.forEach((feature: any) => {
-				if (track.id === feature.id) {
-					track.tempo = feature.tempo;
-					track.acousticness = feature.acousticness;
-					track.energy = feature.energy;
-					track.valence = feature.valence;
-					track.danceability = feature.danceability;
-					track.mode = feature.mode;
-					track.key = feature.key;
-					track.selected = false;
-				}
-			})
-		})
-		setTracks(updatedTracks);
-	}
-	
-	function createPlaylist() {
-		let userID = 'avinladd';
-		let playlistName = 'API Playlist';
-		let playlistDesc = 'This is the API desription';
-		Axios.post("http://localhost:3001/createPlaylist", { accessToken, userID, playlistName, playlistDesc });
-	}
-	
-	function sortTracks(filter: string) {
-		let sortedTracks: Track[];
-		sortedTracks = (sortTracksOrder === 0 || sortTracksOrder === -1) ? tracks.sort(compare(filter)) : tracks.sort(compare(filter)).reverse();
-		
-		setTracks(sortedTracks);
-		setSortTracksOrder((sortTracksOrder === 0 || sortTracksOrder === -1) ? 1 : -1);
-	}
-	
-	function compare(filter: string) {
-		return function (a: any, b: any) {
-			if (typeof a[filter] === 'string') {
-				if (a[filter].toUpperCase() < b[filter].toUpperCase()) { return -1; }
-				else if (a[filter].toUpperCase() > b[filter].toUpperCase()) { return 1; }
-				return 0;
-			} 
-			else if (filter === 'artists') {
-				if (a[filter][0].name.toUpperCase() < b[filter][0].name.toUpperCase()) { return -1; }
-				else if (a[filter][0].name.toUpperCase() > b[filter][0].name.toUpperCase()) { return 1; }
-				return 0;
-			} 
-			else {
-				if (a[filter] < b[filter]) { return -1; }
-				else if (a[filter] > b[filter]) { return 1; }
-				return 0;
-			}
-		}
-	}
-	
-	function formatDate(date: string) {
-		let datetime: Date = new Date(date);
-		return datetime.toLocaleDateString();
-	}
-	
-	// DISPLAY TRACKS
-	function displayTracks() {
-		if (!tracks) return '';
-		let trackComp: any = [];
-		trackComp = tracks.map((track: any) => (
-			<tr key={track.id} style={ {padding: "10px"} }>
-			<td style={{ textAlign: "center" }}><input className="inputButton" id={track.id} name={track.id} type="checkbox" value={track.selected} onChange={() => handleChange(track)}/></td>
-			<td style={{ padding: "5px", minWidth: "30px" }}>{track.name}</td>
-			<td style={{ padding: "5px" }}>{track.artists.map((artist: Artist) => artist.name).join(", ")}</td>
-			<td>{track.tempo}</td>
-			<td>{track.valence}</td>
-			<td>{track.energy}</td>
-			<td>{track.danceability}</td>
-			<td>{track.mode}</td>
-			<td>{track.key}</td>
-			<td style={{ textAlign: "center" }}>{formatDate(track.dateAdded)}</td>
-			</tr>
-			));
-			// reverse function makes it so the order is from newest (top) to lowest (bottom)
-			return trackComp.reverse();
-		}
-		
-		return (  
-			<Container>
-			<div className="text-center text-success">
-			<h2>{playlist ? playlist.name : "No Playlist"}</h2>
-			<p>Total Tracks: {tracks ? tracks.length : "0"}</p>
-			<button type="button" className="btn btn-success btn-lg" onClick={handleSubmit}>Make New Playlist</button>
-			</div>
-			<div className="d-flex justify-content-center">
-			<table className="table text-light">
-			<thead className="center tableHeader">
-			<tr>
-			<th className="headerButton" style={{ textDecoration: "none" }}>Select</th>
-			<th><button className="headerButton" onClick={() => sortOrder('name')}>Song Name⇵</button></th>
-			<th><button className="headerButton" onClick={() => sortOrder('artists')}>Artists⇵</button></th>
-			<th><button className="headerButton" onClick={() => sortOrder('tempo')}>Tempo⇵</button></th>
-			<th><button className="headerButton" onClick={() => sortOrder('valence')}>Valence⇵</button></th>
-			<th><button className="headerButton" onClick={() => sortOrder('energy')}>Energy⇵</button></th>
-			<th><button className="headerButton" onClick={() => sortOrder('danceability')}>Dance⇵</button></th>
-			<th><button className="headerButton" onClick={() => sortOrder('mode')}>Mode⇵</button></th>
-			<th><button className="headerButton" onClick={() => sortOrder('key')}>Key⇵</button></th>
-			<th><button className="headerButton" onClick={() => sortOrder('dateAdded')}>Date Added⇵</button></th>
-			</tr>
-			</thead>
-			<tbody className="">
-			{ tracks ? displayTracks() : '' }
-			</tbody>
-			</table>
-			</div>
-			</Container>
-			);
-		};
-		
-		export default Playlist;
+  let { playlistID } = useParams();
+  const [tracks, setTracks] = useState<Track[]>([]);
+  const [playlist, setPlaylist] = useState<PlaylistObj>();
+  const [sortTracksOrder, setSortTracksOrder] = useState(0);
+  const [selectedTracks, setSelectedTracks] = useState<Track[]>([]);
+  const sortOrder = (filter: string) => sortTracks(filter);
+  const accessToken = sessionStorage.getItem("accessToken");
+
+  useEffect(() => {
+    if (accessToken && playlistID) {
+      Axios.get("http://localhost:3001/getPlaylistInfo", {
+        params: { accessToken, playlistID },
+      })
+        .then((res) => {
+          const pl: PlaylistObj = {
+            id: res.data.id,
+            name: res.data.name,
+            description: res.data.description,
+            uri: res.data.uri,
+            total: res.data.tracks.total,
+          };
+          setPlaylist(pl);
+        })
+        .catch(() => {
+          window.location.href = "/";
+        });
+    }
+  }, [accessToken, playlistID]);
+
+  useEffect(() => {
+    if (playlist && tracks && tracks.length < playlist.total) {
+      const totalTracks = playlist?.total;
+      Axios.get("http://localhost:3001/getPlaylistTracks", {
+        params: { accessToken, playlistID, totalTracks },
+      })
+        .then((res) => {
+          formatTracks(res.data);
+        })
+        .catch(() => {
+          window.location.href = "/";
+        });
+    }
+  });
+
+  const handleChange = (track: Track) => {
+    track.selected = !track.selected;
+    if (!track.selected) {
+      setSelectedTracks((selectedTracks) =>
+        selectedTracks.filter((curTrack) => {
+          return curTrack.id !== track.id;
+        })
+      );
+    } else {
+      let found = selectedTracks.findIndex(
+        (curTrack) => curTrack.id === track.id
+      );
+      if (found === -1) {
+        selectedTracks.push(track);
+      }
+    }
+  };
+
+  const handleSubmit = async () => {
+    createPlaylist();
+  };
+
+  function formatTracks(resTracks: any) {
+    let shapedTracks: Track[] = [];
+    shapedTracks = resTracks.map((track: any) => ({
+      id: track.track.id,
+      name: track.track.name,
+      artists: _formatArtists(track.track.artists),
+      dateAdded: track.added_at,
+      tempo: 0,
+      acousticness: 0,
+      valence: 0,
+      energy: 0,
+      danceability: 0,
+      mode: 0,
+      key: 0,
+    }));
+    audioFeatures(shapedTracks);
+  }
+
+  // shape artsits
+  function _formatArtists(artists: any) {
+    return artists.map(
+      (artist: any): Artist => ({
+        id: artist.id,
+        name: artist.name,
+      })
+    );
+  }
+
+  // async processes to retrieve all audio features
+  async function audioFeatures(rawTracks: Track[]) {
+    if (playlist) {
+      let tracks = rawTracks.map((track) => track.id);
+      let offsets = Math.floor(tracks.length / 100) + 1;
+      let promises = [];
+      let curTracks: string = "";
+      for (let chunk = 0; chunk < offsets; chunk++) {
+        curTracks = tracks.slice(chunk * 100, (chunk + 1) * 100).join(",");
+        promises.push(_getAudioFeatures(curTracks));
+      }
+
+      Promise.all(promises).then((response) => {
+        let audioFeatures: any = [];
+        response.forEach((audioFeature) => {
+          audioFeatures = audioFeatures.concat(audioFeature);
+        });
+        addAudioFeatures(rawTracks, audioFeatures);
+      });
+    }
+  }
+
+  // get request for audio features
+  async function _getAudioFeatures(tracksID: string) {
+    let res = await Axios.get("http://localhost:3001/getTrackFeatures", {
+      params: { accessToken, tracksID },
+    });
+    if (res.status !== 200) {
+      return null;
+    }
+    return res.data;
+  }
+
+  // add audio features to state
+  function addAudioFeatures(shapedTracks: Track[], audioFeatures: any) {
+    let updatedTracks: Track[] = shapedTracks;
+    if (!tracks && !audioFeatures) {
+      return null;
+    }
+    if (updatedTracks.length !== audioFeatures.length) {
+      return null;
+    }
+    updatedTracks.forEach((track) => {
+      audioFeatures.forEach((feature: any) => {
+        if (track.id === feature.id) {
+          track.tempo = feature.tempo;
+          track.acousticness = feature.acousticness;
+          track.energy = feature.energy;
+          track.valence = feature.valence;
+          track.danceability = feature.danceability;
+          track.mode = feature.mode;
+          track.key = feature.key;
+          track.selected = false;
+        }
+      });
+    });
+    setTracks(updatedTracks);
+  }
+
+  function createPlaylist() {
+    let userID = "avinladd";
+    let playlistName = "API Playlist";
+    let playlistDesc = "This is the API desription";
+    Axios.post("http://localhost:3001/createPlaylist", {
+      accessToken,
+      userID,
+      playlistName,
+      playlistDesc,
+    });
+  }
+
+  function sortTracks(filter: string) {
+    let sortedTracks: Track[];
+    sortedTracks =
+      sortTracksOrder === 0 || sortTracksOrder === -1
+        ? tracks.sort(compare(filter))
+        : tracks.sort(compare(filter)).reverse();
+
+    setTracks(sortedTracks);
+    setSortTracksOrder(
+      sortTracksOrder === 0 || sortTracksOrder === -1 ? 1 : -1
+    );
+  }
+
+  function compare(filter: string) {
+    return function (a: any, b: any) {
+      if (typeof a[filter] === "string") {
+        if (a[filter].toUpperCase() < b[filter].toUpperCase()) {
+          return -1;
+        } else if (a[filter].toUpperCase() > b[filter].toUpperCase()) {
+          return 1;
+        }
+        return 0;
+      } else if (filter === "artists") {
+        if (a[filter][0].name.toUpperCase() < b[filter][0].name.toUpperCase()) {
+          return -1;
+        } else if (
+          a[filter][0].name.toUpperCase() > b[filter][0].name.toUpperCase()
+        ) {
+          return 1;
+        }
+        return 0;
+      } else {
+        if (a[filter] < b[filter]) {
+          return -1;
+        } else if (a[filter] > b[filter]) {
+          return 1;
+        }
+        return 0;
+      }
+    };
+  }
+
+  function formatDate(date: string) {
+    let datetime: Date = new Date(date);
+    return datetime.toLocaleDateString();
+  }
+
+  // DISPLAY TRACKS
+  function displayTracks() {
+    if (!tracks) return "";
+    let trackComp: any = [];
+    trackComp = tracks.map((track: any) => (
+      <tr key={track.id} style={{ padding: "10px" }}>
+        <td style={{ textAlign: "center" }}>
+          <input
+            className="inputButton"
+            id={track.id}
+            name={track.id}
+            type="checkbox"
+            value={track.selected}
+            onChange={() => handleChange(track)}
+          />
+        </td>
+        <td style={{ padding: "5px", minWidth: "30px" }}>{track.name}</td>
+        <td style={{ padding: "5px" }}>
+          {track.artists.map((artist: Artist) => artist.name).join(", ")}
+        </td>
+        <td>{track.tempo}</td>
+        <td>{track.valence}</td>
+        <td>{track.energy}</td>
+        <td>{track.danceability}</td>
+        <td>{track.mode}</td>
+        <td>{track.key}</td>
+        <td style={{ textAlign: "center" }}>{formatDate(track.dateAdded)}</td>
+      </tr>
+    ));
+    // reverse function makes it so the order is from newest (top) to lowest (bottom)
+    return trackComp.reverse();
+  }
+
+  return (
+    <Container>
+      <div className="text-center text-success">
+        <h2>{playlist ? playlist.name : "No Playlist"}</h2>
+        <p>Total Tracks: {tracks ? tracks.length : "0"}</p>
+        <button
+          type="button"
+          className="btn btn-success btn-lg"
+          onClick={handleSubmit}
+        >
+          Make New Playlist
+        </button>
+      </div>
+      <div className="d-flex justify-content-center">
+        <table className="table text-light">
+          <thead className="center tableHeader">
+            <tr>
+              <th className="headerButton" style={{ textDecoration: "none" }}>
+                Select
+              </th>
+              <th>
+                <button
+                  className="headerButton"
+                  onClick={() => sortOrder("name")}
+                >
+                  Song Name⇵
+                </button>
+              </th>
+              <th>
+                <button
+                  className="headerButton"
+                  onClick={() => sortOrder("artists")}
+                >
+                  Artists⇵
+                </button>
+              </th>
+              <th>
+                <button
+                  className="headerButton"
+                  onClick={() => sortOrder("tempo")}
+                >
+                  Tempo⇵
+                </button>
+              </th>
+              <th>
+                <button
+                  className="headerButton"
+                  onClick={() => sortOrder("valence")}
+                >
+                  Valence⇵
+                </button>
+              </th>
+              <th>
+                <button
+                  className="headerButton"
+                  onClick={() => sortOrder("energy")}
+                >
+                  Energy⇵
+                </button>
+              </th>
+              <th>
+                <button
+                  className="headerButton"
+                  onClick={() => sortOrder("danceability")}
+                >
+                  Dance⇵
+                </button>
+              </th>
+              <th>
+                <button
+                  className="headerButton"
+                  onClick={() => sortOrder("mode")}
+                >
+                  Mode⇵
+                </button>
+              </th>
+              <th>
+                <button
+                  className="headerButton"
+                  onClick={() => sortOrder("key")}
+                >
+                  Key⇵
+                </button>
+              </th>
+              <th>
+                <button
+                  className="headerButton"
+                  onClick={() => sortOrder("dateAdded")}
+                >
+                  Date Added⇵
+                </button>
+              </th>
+            </tr>
+          </thead>
+          <tbody className="">{tracks ? displayTracks() : ""}</tbody>
+        </table>
+      </div>
+    </Container>
+  );
+};
+
+export default Playlist;
