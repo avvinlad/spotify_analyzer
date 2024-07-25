d25e90fc073c2332e79758a403ad464d9c4bbe1e
Avin Lad <avinlad@gmail.com>
2024-07-25 19:01:58 -0400

    set theme to dark and created dark background

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,6 +5,8 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<App />
+		<div className="dark bg-neutral-600">
+			<App />
+		</div>
 	</React.StrictMode>
 );
